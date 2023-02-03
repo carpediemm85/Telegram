b983d6e5d2159da6ee1e3598458138dc34c823be
carpediemm85 <rustem.abbaszade@gmail.com>
2023-02-03 23:20:11 +0400

    first commit

---
+++ forGasa.py
@@ -0,0 +1,35 @@
+from telebot import types,telebot
+
+bot = telebot.TeleBot('6040147356:AAEoZ4ksAsTXI9ueC-SWoQ3zMDud9z041Jc')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton("Oz hesabina basvuru") 
+    btn2 = types.KeyboardButton("Guz donemi")
+    btn3 = types.KeyboardButton("Burs icin gerekli belgeler")
+    btn4 = types.KeyboardButton("Dim senedleri ucun")
+    btn5 = types.KeyboardButton("Çin")
+    btn6 = types.KeyboardButton("Amerika")
+    
+    markup.add(btn1,btn2,btn3,btn4,btn5,btn6)
+    
+    send_mess = f"<b>Salam {message.from_user.first_name}!</b>\nNe lazimdii"
+
+    bot.send_message(message.chat.id, send_mess, parse_mode="html", reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    finalMessage = ""
+
+    get_message_bot = message.text.strip()
+    if get_message_bot == "Oz hesabina basvuru":
+        finalMessage='https://gasacompany.com/bahar-donem-basvuru-icin-gerekli-belgeler/'
+    elif get_message_bot == 'Guz donemi':
+        finalMessage='https://gasacompany.com/bahar-donem-basvuru-icin-gerekli-belgeler'
+    elif get_message_bot=="Burs icin gerekli belgeler":
+        finalMessage='https://docs.google.com/document/d/1D42xwi2uvDn5f4K6YQPkeVTUF5w9belq/edit'
+    bot.send_message(message.chat.id, finalMessage, parse_mode='html')        
+bot.polling(none_stop=True)
+
