82100ed099d0a91b017ffd6ef430c47cc834eef8
carpediemm85 <rustem.abbaszade@gmail.com>
2023-02-05 00:25:54 +0400

    added files

--- forGasa.py
+++ forGasa.py
@@ -1,24 +1,28 @@
 from telebot import types,telebot
 import os
-from dotenv import load_dotenv
-load_dotenv()
 import api
-
+import yok
 bot = telebot.TeleBot(api.API_TOKEN)
+path = "./refs"
+
+img = open("./docs/card.jpeg", 'rb')
+guz = open("./docs/senedler.pdf","rb")
+steps = open("./docs/addımlar.pdf","rb")
+
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn1 = types.KeyboardButton("Oz hesabina basvuru") 
-    btn2 = types.KeyboardButton("Guz donemi")
-    btn3 = types.KeyboardButton("Burs icin gerekli belgeler")
-    btn4 = types.KeyboardButton("Dim senedleri ucun")
-    btn5 = types.KeyboardButton("Çin")
-    btn6 = types.KeyboardButton("Gasa")
-    
+    btn1 = types.KeyboardButton("Öz hesabına başvuru") 
+    btn2 = types.KeyboardButton("Burs")
+    btn3 = types.KeyboardButton("DİM sənədləri")
+    btn4 = types.KeyboardButton("OTB")
+    btn5 = types.KeyboardButton("Yeni qoşulanlar üçün")
+    btn6 = types.KeyboardButton("Nümunələr")
     markup.add(btn1,btn2,btn3,btn4,btn5,btn6)
-    
-    send_mess = f"<b>Salam {message.from_user.first_name}!</b>\nNe lazimdii"
+    send_mess = f"<b>Salam {message.from_user.first_name}!</b>\nNe lazimdır?"
+    send_mess = f"<b>Salam {message.from_user.first_name}!</b>\nNe lazimdır?"
 
     bot.send_message(message.chat.id, send_mess, parse_mode="html", reply_markup=markup)
 
@@ -26,14 +30,30 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def mess(message):
     finalMessage = ""
-
     get_message_bot = message.text.strip()
-    if get_message_bot == "Oz hesabina basvuru":
-        finalMessage='https://gasacompany.com/bahar-donem-basvuru-icin-gerekli-belgeler/'
-    elif get_message_bot == 'Guz donemi':
-        finalMessage='https://gasacompany.com/bahar-donem-basvuru-icin-gerekli-belgeler'
-    elif get_message_bot=="Burs icin gerekli belgeler":
-        finalMessage='https://docs.google.com/document/d/1D42xwi2uvDn5f4K6YQPkeVTUF5w9belq/edit'
+  
+    if get_message_bot == "Öz hesabına başvuru":
+        bot.send_document(message.chat.id, guz)
+  
+    elif get_message_bot == 'Burs':
+        bot.send_document(message.chat.id, document=open('./docs/BursRec.docx', 'rb'))
+
+    elif get_message_bot=="DİM sənədləri":
+        finalMessage='https://eservices.dim.gov.az/sagird/indexsh.aspx?frm=egov'
+    
+    elif get_message_bot=="OTB":
+        finalMessage=yok.message
+        bot.send_photo(message.chat.id, photo=open('./docs/card.jpeg', 'rb'))
+
+    elif get_message_bot=='Yeni qoşulanlar üçün':
+        bot.send_document(message.chat.id, steps)
+
+    elif get_message_bot == "Nümunələr":
+        
+        filelist = os.listdir(path)
+        for x in filelist:
+            bot.send_document(message.chat.id,document=open(path+"/"+x,'rb'))
+
     bot.send_message(message.chat.id, finalMessage, parse_mode='html')        
 bot.polling(none_stop=True)
 
---
+++ yok.py
@@ -0,0 +1,11 @@
+message = """Okul Tanıma Belgesi  Hal hazırda təhsil aldığınız Universitetin Türkiyədə tanınmasını təsdiqləyən sənəddir . 
+Bu sənəd Ankaradan YÖK’dən ( Yüksek Öğretim Kurulu ) alınır. Dövlət rüsüm , vergi və yol xərcləri ilə birlikdə toplam 50 azn ödəmə edəcəksiz.
+Lazım olan sənədləri info@gasacompany.com adresinə göndərin zəhmət olmasa .                 
+
+1. Pasport fotokopi                                     
+2. Hal hazırda təhsil aldığınız Universitetin adını yazırsız                                                     
+3. Ödəmə qəbzi                                                   
+
+Qeyd : Mail göndərdikdə açıkla qisminə Okul Tanıma yazmağı unutmayın .                                                             
+
+Ödəməni qısa zamanda həll edərsiniz ki ilk sənədiniz tam hazır olsun zamanında."""
